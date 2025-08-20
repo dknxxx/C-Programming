022ab52dfc656672f490d8998320046ad0090a6f
E570 <15959539+dknxxx@users.noreply.github.com>
2025-08-19 21:55:22 -0500

    Finished ch9/mergesort.c

--- ch9/mergesort.c
+++ ch9/mergesort.c
@@ -1,30 +1,54 @@
 #include <stdio.h>
 
-#define N 2
+#define N 4
 
-void sort(int a[], int low, int high) {
-	int i, j, temp;
-	
-	for (i = low; i < high; i++) {
-		for (j = low + i; j < high; j++) {
-			if (a[i] > a[j]) {
-				temp = a[i];
-				a[i] = a[j];
-				a[j] = temp;
+void sort(int a[], int low, int mid, int high) {
+	int n1 = mid - low + 1;
+	int n2 = high - mid;
 
-			}
+	int left[n1], right[n2];
+
+	for (int i = 0; i < n1; i++) {
+		left[i] = a[low + i];
+	}
+
+	for (int i = 0; i < n2; i++) {
+		right[i] = a[mid + i + 1];
+	}
+
+	int i = 0; int j = 0; int k = low;
+
+	while (i < n1 && j < n2) {
+		if (left[i] <= right[j]) {
+			a[k] = left[i];
+			i++;
+		} else {
+			a[k] = right[j];
+			j++;
 		}
+		k++;
+	}	
+	while (i < n1) {
+		a[k] = left[i];
+		i++;
+		k++;
+	}
+
+	while (j < n2) {
+		a[k] = right[j];
+		j++;
+		k++;
 	}
 }
 
 void merge(int a[], int low, int high) {
-	int middle = (low + high) / 2;
-	
-	if (middle <= 1) return;
+	if (low < high) {
+	int mid = low + (high - low) / 2;
 
-	merge(a, low, middle - 1);
-	merge(a, middle, high);
-	sort(a, low, high);
+	merge(a, low, mid);
+	merge(a, mid + 1, high);
+	sort(a, low, mid, high);
+	}
 }
 
 int main(void) {
@@ -34,13 +58,16 @@ int main(void) {
 	while (i < N) 
 		scanf("%d", &a[i++]);
 
-	merge(a, 0, N);
+	merge(a, 0, N - 1);
 	
 	printf("In sorted order: ");
 	i = 0;
-	while (i < N) 
+	while (i < N) {
 		printf("%d ", a[i]);
+		i++;
+	}
 	printf("\n");
 
 	return 0;
+
 }
